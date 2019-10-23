0504da1ba5641ae69fce9cddb08adb0ecb9e5e2c
JOC <joc.neves@gmail.com>
2019-10-23 16:04:34 -0300

    api 1.1

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ require('./configs/passport');
 // IF YOU STILL DIDN'T, GO TO 'configs/passport.js' AND UN-COMMENT OUT THE WHOLE FILE
 
 mongoose
-  .connect('mongodb://localhost/project-management-server', {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true
   })
   .then(x => {
